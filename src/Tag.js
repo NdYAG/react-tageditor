3fbcb300132daa5c4730ec44d827f1406bd033d2
Akshay090 <aks28id@gmail.com>
2019-08-10 13:04:10 +0530

    added method to get current edit value of tag

--- src/Tag.js
+++ src/Tag.js
@@ -1,129 +1,142 @@
-import React, { Component } from "react"
-import { findDOMNode } from 'react-dom'
-import { KEYS } from "./const"
-import utils from "./utils"
+import React, { Component } from "react";
+import { findDOMNode } from "react-dom";
+import { KEYS } from "./const";
+import utils from "./utils";
 
 class Tag extends Component {
-    constructor(props) {
-        super(props)
-        this.inputRef = React.createRef()
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+  componentDidMount() {
+    var node = this.inputRef.current;
+    if (this.props.active) {
+      node.focus();
+    } else {
+      node.blur();
     }
-    componentDidMount() {
-        var node = this.inputRef.current
-        if (this.props.active) {
-            node.focus()
-        } else {
-            node.blur()
-        }
-        utils.autoSize(node)
-    }
-    componentWillReceiveProps(nextProps) {
-        var node = this.inputRef.current
-          , tagNode = findDOMNode(this)
-          , activityChanged = this.props.active !== nextProps.active
+    utils.autoSize(node);
+  }
+  componentWillReceiveProps(nextProps) {
+    var node = this.inputRef.current,
+      tagNode = findDOMNode(this),
+      activityChanged = this.props.active !== nextProps.active;
 
-        if (nextProps.active) {
-            utils.setCaretPos(node, nextProps.caret)
-        } else {
-            node.blur()
-        }
+    if (nextProps.active) {
+      utils.setCaretPos(node, nextProps.caret);
+    } else {
+      node.blur();
     }
-    handleClick(e) {
-        e.stopPropagation()
-        if (e.target.tagName === 'A') {
-            // click on X to remove
-            this.props.onRemove()
-        }
+  }
+  handleClick(e) {
+    e.stopPropagation();
+    if (e.target.tagName === "A") {
+      // click on X to remove
+      this.props.onRemove();
     }
-    handleFocus() {
-        this.props.onFocus()
+  }
+  handleFocus() {
+    this.props.onFocus();
+  }
+  handleBlur() {
+    var node = this.inputRef.current;
+    this.props.onSave(node.value);
+  }
+  handleKeyDown(e) {
+    var charCode = utils.getCharcode(e);
+    for (var key in KEYS) {
+      if (KEYS[key] === charCode) {
+        keyHandlers[key].call(this, {
+          originalEvent: e,
+          caret: utils.getCaretPos(this.inputRef.current),
+          node: this.inputRef.current
+        });
+      }
     }
-    handleBlur() {
-        var node = this.inputRef.current
-        this.props.onSave(node.value)
+    if (this.props.delimiterKeys.indexOf(charCode) > -1) {
+      e.preventDefault();
+      var node = this.inputRef.current;
+      this.split(node, utils.getCaretPos(node));
     }
-    handleKeyDown(e) {
-        var charCode = utils.getCharcode(e)
-        for (var key in KEYS) {
-            if (KEYS[key] === charCode) {
-                keyHandlers[key].call(this, {
-                    originalEvent: e,
-                    caret: utils.getCaretPos(this.inputRef.current),
-                    node: this.inputRef.current
-                })
-            }
-        }
-        if (this.props.delimiterKeys.indexOf(charCode) > -1) {
-            e.preventDefault()
-            var node = this.inputRef.current
-            this.split(node, utils.getCaretPos(node))
-        }
-    }
-    handleChange(e) {
-        var tagText = e.target.value
-          , node = this.inputRef.current
-          , caretPos = utils.getCaretPos(node)
-          , lastInput = tagText.charAt(caretPos - 1)
-        this.props.delimiterChars.forEach(delimiter =>  {
-            if (lastInput === delimiter) {
-                this.split(node, caretPos - 1, caretPos)
-            }
-        })
-        utils.autoSize(node)
-    }
-    split(node) {
-        var positions = Array.prototype.slice.call(arguments, 1)
-          , tagText = node.value
-          , textBeforeCaret = tagText.substring(0, positions[0])
-          , textAfterCaret = tagText.substring(positions[1] || positions[0], tagText.length)
-        node.value = textBeforeCaret
-        node.blur()
-        this.props.onSplit(textBeforeCaret, textAfterCaret)
-    }
-    render() {
-        return (
-            <div className={"tag" + (this.props.error? " has-error": "")} onClick={this.handleClick.bind(this)}>
-                <input
-                    type="text"
-                    defaultValue={this.props.children.toString()}
-                    ref={this.inputRef}
-                    onFocus={this.handleFocus.bind(this)}
-                    onBlur={this.handleBlur.bind(this)}
-                    onKeyDown={this.handleKeyDown.bind(this)}
-                    onChange={this.handleChange.bind(this)} />
-                <a className="">&times;</a>
-            </div>
-        )
-    }
+  }
+  handleChange(e) {
+    var tagText = e.target.value,
+      node = this.inputRef.current,
+      caretPos = utils.getCaretPos(node),
+      lastInput = tagText.charAt(caretPos - 1);
+
+    this.props.currentTagTxt(tagText);
+
+    
+    this.props.delimiterChars.forEach(delimiter => {
+      if (lastInput === delimiter) {
+        this.split(node, caretPos - 1, caretPos);
+      }
+    });
+    utils.autoSize(node);
+  }
+  split(node) {
+    var positions = Array.prototype.slice.call(arguments, 1),
+      tagText = node.value,
+      textBeforeCaret = tagText.substring(0, positions[0]),
+      textAfterCaret = tagText.substring(
+        positions[1] || positions[0],
+        tagText.length
+      );
+    node.value = textBeforeCaret;
+    node.blur();
+    this.props.onSplit(textBeforeCaret, textAfterCaret);
+  }
+  render() {
+    return (
+      <div
+        className={"tag" + (this.props.error ? " has-error" : "")}
+        onClick={this.handleClick.bind(this)}
+      >
+        <input
+          type="text"
+          defaultValue={this.props.children.toString()}
+          ref={this.inputRef}
+          onFocus={this.handleFocus.bind(this)}
+          onBlur={this.handleBlur.bind(this)}
+          onKeyDown={this.handleKeyDown.bind(this)}
+          onChange={this.handleChange.bind(this)}
+        />
+        <a className="">&times;</a>
+      </div>
+    );
+  }
 }
 
 var keyHandlers = {
-        LEFT: function(e) {
-            if (e.caret === 0) {
-                // seems that:
-                // if current node do not blur
-                // can not set next node to focus
-                e.node.blur()
-                e.originalEvent.preventDefault()
-                this.props.onBlur(e.caret, KEYS.LEFT)
-            }
-        },
-        RIGHT: function(e) {
-            if (e.caret === e.node.value.length) {
-                e.node.blur()
-                e.originalEvent.preventDefault()
-                this.props.onBlur(e.caret, KEYS.RIGHT)
-            }
-        },
-        BACKSPACE: function(e) {
-            if (e.caret === 0) {
-                var node = e.node
-                if (node.selectionStart !== node.selectionEnd) { return }
-                e.node.blur()
-                e.originalEvent.preventDefault()
-                this.props.onBlur(e.caret, KEYS.LEFT)
-            }
-        }
-}
+  LEFT: function(e) {
+    if (e.caret === 0) {
+      // seems that:
+      // if current node do not blur
+      // can not set next node to focus
+      e.node.blur();
+      e.originalEvent.preventDefault();
+      this.props.onBlur(e.caret, KEYS.LEFT);
+    }
+  },
+  RIGHT: function(e) {
+    if (e.caret === e.node.value.length) {
+      e.node.blur();
+      e.originalEvent.preventDefault();
+      this.props.onBlur(e.caret, KEYS.RIGHT);
+    }
+  },
+  BACKSPACE: function(e) {
+    if (e.caret === 0) {
+      var node = e.node;
+      if (node.selectionStart !== node.selectionEnd) {
+        return;
+      }
+      e.node.blur();
+      e.originalEvent.preventDefault();
+      this.props.onBlur(e.caret, KEYS.LEFT);
+    }
+  }
+};
 
-export default Tag
+export default Tag;
--- src/TagEditor.js
+++ src/TagEditor.js
@@ -1,173 +1,207 @@
-import { KEYS, ERROR } from "./const"
-import React, { Component } from "react"
-import { connectToStore } from "./connectToStore"
-import Tag from "./Tag"
+import { KEYS, ERROR } from "./const";
+import React, { Component } from "react";
+import { connectToStore } from "./connectToStore";
+import Tag from "./Tag";
 
 class TagEditor extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            editing: null,
-            caret: null,
-            repeat: null
-        }
-    }
-    handleClick() {
-        this.props.store.add('', function(err, newTag, indexOfNewTag) {
-            this.setState({
-                editing: indexOfNewTag,
-                caret: 0
-            })
-        }.bind(this))
-    }
-    handleTagSave(tag, text) {
-        this.props.store.save(tag, text, function(err) {
-            if (!err) {
-                this.props.onChange && this.props.onChange(text, this.props.store.output(), 'add')
-                return
-            }
-            switch(err.name) {
-                case ERROR.EMPTY:
-                    break
-                case ERROR.REPEAT:
-                    var tagIndex
-                    this.props.store.tags.forEach((t, i) => {
-                        if (t.text === text) {
-                            tagIndex = i
-                        }
-                    })
-                    this.setState({
-                        repeat: tagIndex
-                    })
-                    setTimeout(() => {
-                        this.setState({ repeat: null })
-                    }, 1500)
-                    break
-                default:
-                    break
-            }
-            this.props.onError && this.props.onError(err)
-            this.handleTagRemove(tag)
-        }.bind(this))
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: null,
+      caret: null,
+      repeat: null
+    };
+  }
+  handleClick() {
+    this.props.store.add(
+      "",
+      function(err, newTag, indexOfNewTag) {
         this.setState({
-            editing: null,
-            caret: null
-        })
-    }
-    handleTagFocus(tag) {
-        var tagIndex
-        this.props.store.tags.forEach((t, i) => {
-            if (tag.id === t.id) {
-                tagIndex = i
-            }
-        })
-        if (tagIndex === void 0) { return }
-        this.setState({
-            editing: tagIndex
-        })
-    }
-    handleTagBlur(tag, caretOnBlur, lastKeyOnBlur) {
-        var tags = this.props.store.tags
-          , tagIndex
-
-        if (!tags.length) {
-            this.handleClick()
-            return
+          editing: indexOfNewTag,
+          caret: 0
+        });
+      }.bind(this)
+    );
+  }
+  handleTagSave(tag, text) {
+    this.props.store.save(
+      tag,
+      text,
+      function(err) {
+        if (!err) {
+          this.props.onChange &&
+            this.props.onChange(text, this.props.store.output(), "add");
+          return;
         }
-        tags.forEach((t, i) => {
-            if (t.id === tag.id) {
-                tagIndex = i
-            }
-        })
-        if (caretOnBlur === 0) {
-            if (tagIndex > 0) {
-                this.setState({
-                    editing: tagIndex - 1,
-                    caret: tags[tagIndex - 1].text.length
-                })
-            } else if (tagIndex === 0) {
-                this.setState({
-                    editing: 0,
-                    caret: 0
-                })
-            } else {
-                // tagIndex === void 0
-                // this case happens when
-                // press left/right/backspace key on empty tag
-                var newState = {}
-                if (lastKeyOnBlur === KEYS.LEFT) {
-                    if (this.state.editing - 1 < 0) { return }
-                    newState.editing = this.state.editing - 1
-                    newState.caret = tags[newState.editing].text.length
-                }
-                if (lastKeyOnBlur === KEYS.RIGHT) {
-                    newState.editing = this.state.editing
-                    newState.caret = 0
-                }
-                this.setState(newState)
-            }
-        }
-        if (caretOnBlur > 0) {
-            if (tagIndex < tags.length - 1) {
-                this.setState({
-                    editing: tagIndex + 1,
-                    caret: 0
-                })
-            } else if (tagIndex === tags.length - 1) {
-                this.setState({
-                    editing: tags.length - 1,
-                    caret: tags[tags.length - 1].text.length
-                })
-            }
-        }
-    }
-    handleTagRemove(tag) {
-        var tagText = tag.text
-        this.props.store.remove(tag)
-        tagText.length && this.props.onChange && this.props.onChange(tagText, this.props.store.output(), 'remove')
-    }
-    handleTagSplit(tag, textBeforeCaret, textAfterCaret) {
-        this.props.store.insertAfterTag(tag, textAfterCaret, function(err, newTag, indexOfNewTag) {
+        switch (err.name) {
+          case ERROR.EMPTY:
+            break;
+          case ERROR.REPEAT:
+            var tagIndex;
+            this.props.store.tags.forEach((t, i) => {
+              if (t.text === text) {
+                tagIndex = i;
+              }
+            });
             this.setState({
-                editing: indexOfNewTag,
-                caret: 0
-            })
-        }.bind(this))
-    }
-    render() {
-        var store = this.props.store
-        if (!store) {
-            return <div />
+              repeat: tagIndex
+            });
+            setTimeout(() => {
+              this.setState({ repeat: null });
+            }, 1500);
+            break;
+          default:
+            break;
         }
+        this.props.onError && this.props.onError(err);
+        this.handleTagRemove(tag);
+      }.bind(this)
+    );
+    this.setState({
+      editing: null,
+      caret: null
+    });
+  }
+  handleTagFocus(tag) {
+    var tagIndex;
+    this.props.store.tags.forEach((t, i) => {
+      if (tag.id === t.id) {
+        tagIndex = i;
+      }
+    });
+    if (tagIndex === void 0) {
+      return;
+    }
+    this.setState({
+      editing: tagIndex
+    });
+  }
+  handleTagBlur(tag, caretOnBlur, lastKeyOnBlur) {
+    var tags = this.props.store.tags,
+      tagIndex;
 
-        var tags = this.props.store.tags
-        var tagNodes = tags.map((tag, i) => {
-            return (
-                <Tag
-                    active={this.state.editing === i? true: false}
-                    error={this.state.repeat === i? true: false}
-                    caret={this.state.editing === i? this.state.caret: null}
-                    onSave={this.handleTagSave.bind(this, tag)}
-                    onFocus={this.handleTagFocus.bind(this, tag)}
-                    onBlur={this.handleTagBlur.bind(this, tag)}
-                    onRemove={this.handleTagRemove.bind(this, tag)}
-                    onSplit={this.handleTagSplit.bind(this, tag)}
-                    delimiterKeys={this.props.delimiters.filter( d => { return typeof d === 'number'} )}
-                    delimiterChars={this.props.delimiters.filter( d=> { return typeof d === 'string'} )}
-                    key={tag.id}>
-                    {tag.text}
-                </Tag>
-            )
-        })
-        if (!tags.length) {
-            tagNodes = <div className="tag-editor-placeholder">{this.props.placeholder}</div>
-        }
-        return (
-            <div className={"tag-editor" + (typeof this.state.editing === 'number'? " is-active": "")} onClick={this.handleClick.bind(this)}>
-                {tagNodes}
-            </div>
-        )
+    if (!tags.length) {
+      this.handleClick();
+      return;
     }
+    tags.forEach((t, i) => {
+      if (t.id === tag.id) {
+        tagIndex = i;
+      }
+    });
+    if (caretOnBlur === 0) {
+      if (tagIndex > 0) {
+        this.setState({
+          editing: tagIndex - 1,
+          caret: tags[tagIndex - 1].text.length
+        });
+      } else if (tagIndex === 0) {
+        this.setState({
+          editing: 0,
+          caret: 0
+        });
+      } else {
+        // tagIndex === void 0
+        // this case happens when
+        // press left/right/backspace key on empty tag
+        var newState = {};
+        if (lastKeyOnBlur === KEYS.LEFT) {
+          if (this.state.editing - 1 < 0) {
+            return;
+          }
+          newState.editing = this.state.editing - 1;
+          newState.caret = tags[newState.editing].text.length;
+        }
+        if (lastKeyOnBlur === KEYS.RIGHT) {
+          newState.editing = this.state.editing;
+          newState.caret = 0;
+        }
+        this.setState(newState);
+      }
+    }
+    if (caretOnBlur > 0) {
+      if (tagIndex < tags.length - 1) {
+        this.setState({
+          editing: tagIndex + 1,
+          caret: 0
+        });
+      } else if (tagIndex === tags.length - 1) {
+        this.setState({
+          editing: tags.length - 1,
+          caret: tags[tags.length - 1].text.length
+        });
+      }
+    }
+  }
+  handleTagRemove(tag) {
+    var tagText = tag.text;
+    this.props.store.remove(tag);
+    tagText.length &&
+      this.props.onChange &&
+      this.props.onChange(tagText, this.props.store.output(), "remove");
+  }
+  handleTagSplit(tag, textBeforeCaret, textAfterCaret) {
+    this.props.store.insertAfterTag(
+      tag,
+      textAfterCaret,
+      function(err, newTag, indexOfNewTag) {
+        this.setState({
+          editing: indexOfNewTag,
+          caret: 0
+        });
+      }.bind(this)
+    );
+  }
+  render() {
+    var store = this.props.store;
+    if (!store) {
+      return <div />;
+    }
+
+    var tags = this.props.store.tags;
+    var tagNodes = tags.map((tag, i) => {
+      return (
+        <Tag
+          active={this.state.editing === i ? true : false}
+          error={this.state.repeat === i ? true : false}
+          caret={this.state.editing === i ? this.state.caret : null}
+          onSave={this.handleTagSave.bind(this, tag)}
+          onFocus={this.handleTagFocus.bind(this, tag)}
+          onBlur={this.handleTagBlur.bind(this, tag)}
+          onRemove={this.handleTagRemove.bind(this, tag)}
+          onSplit={this.handleTagSplit.bind(this, tag)}
+          currentTagTxt={tagInput => {
+            this.props.currentTagTxt(tagInput);
+          }}
+          delimiterKeys={this.props.delimiters.filter(d => {
+            return typeof d === "number";
+          })}
+          delimiterChars={this.props.delimiters.filter(d => {
+            return typeof d === "string";
+          })}
+          key={tag.id}
+        >
+          {tag.text}
+        </Tag>
+      );
+    });
+    if (!tags.length) {
+      tagNodes = (
+        <div className="tag-editor-placeholder">{this.props.placeholder}</div>
+      );
+    }
+    return (
+      <div
+        className={
+          "tag-editor" +
+          (typeof this.state.editing === "number" ? " is-active" : "")
+        }
+        onClick={this.handleClick.bind(this)}
+      >
+        {tagNodes}
+      </div>
+    );
+  }
 }
 
-export default connectToStore(TagEditor)
+export default connectToStore(TagEditor);
